b809b38a8b0f44f6134a2e107ad244fd03d62e92
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-06-17 21:18:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,10 +1,9 @@
-var playlist = {artistName: "songTitle"};
-
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
-  return playlist
+var playlist = {'artistName':'songTitle'};
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName]=songTitle;
+  return playlist;
 }
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName]
-  return playlist
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
 }
